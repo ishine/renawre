abdaf28e1d85c1ab62aca673e4ebd2fa74c1a612
Mettatw <mettatw@users.noreply.github.com>
2016-08-01 15:59:23 +0800

    First version of lexicon datatype

---
+++ def/lex.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Lexicon - another special case for table
+#***************************************************************************
+#  Copyright 2014-2016, mettatw
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#***************************************************************************
+
+source !.def/table.sh
+
+# ========== Base object and flow function ========== #
+
+function POGDef::lex {
+  local classname="$FUNCNAME"
+  local objname="$1"
+
+  POGDef::table "$objname"
+  pogInjectMethods "$classname" "$objname"
+}
+
+# ========== Write data ========== #
+
+function POGDef::lex::sink {
+  local this="${!1}"
+  local idGetter="${2:-get}"
+  LC_ALL=C sort -b -k1,1 -k2,2nr \
+    | gawk '{pron=""; for(i=4;i<=NF;i++){pron = pron " " $i}} !a[$1 " " pron]++' \
+    | gzip -nc9 > "${this}/table.gz"
+}
