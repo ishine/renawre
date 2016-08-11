2ca96a5b6c79d9f6c78b337e36580b6b3f3a005f
Mettatw <mettatw@users.noreply.github.com>
2016-08-11 18:31:48 +0800

    grep shouldn't return false when results empty

--- def/table.sh
+++ def/table.sh
@@ -97,7 +97,7 @@ function POGDef::table::getNLine {
   if [[ -f "$dir/_meta_nlines" && $forced != 1 ]]; then
     cat "$dir/_meta_nlines"
   else
-    $this::get | grep -v '^ ' | wc -l
+    $this::get | (grep -v '^ ' || true) | wc -l
   fi
   return 0
 }
@@ -106,7 +106,7 @@ function POGDef::table::transpose {
   local this="$1"
   local startcol="${2:-2}"
   $this::get \
-    | grep -v '^ ' \
+    | (grep -v '^ ' || true) \
     | awk -v startcol=$startcol '{for(i=startcol; i<=NF; i++) lst[$i]=lst[$i] " " $1} END {for (k in lst) print k lst[k]}'
 }
 
