636095045deda0333c7e86e75e0f41e2d3860d9e
Mettatw <mettatw@users.noreply.github.com>
2016-08-23 10:34:36 +0800

    Table auto-write numberer for later fst use

--- def/table.sh
+++ def/table.sh
@@ -41,6 +41,11 @@ function POGDef::table::preCheck {
   return 1;
 }
 
+function POGDef::table::postProcess {
+  local this="$1"
+  $this::writeNumberer
+}
+
 function POGDef::table::saveMetadata {
   local this="${1}"
   $this::getNLine 1 > "${!this}/_meta_nlines"
@@ -89,6 +94,31 @@ function POGDef::table::getGetter {
 }
 
 # ========== Type-specific ========== #
+
+# Write a shell script to output a numbering table
+function POGDef::table::writeNumberer {
+  local this="$1"
+  $this::initializeGetter num
+  (
+    cat <<"EOF"
+if [[ "${1:-}" == --nospecial ]]; then
+  shift
+  startnum=1
+  printf '%s 0\n' '<eps>'
+else
+  startnum=11
+  printf '%s 0\n%s 7\n%s 8\n%s 9\n' '<eps>' '<phi>' '<sigma>' '<rho>'
+fi
+numdisambig="${1:-0}"
+EOF
+    $this::getRelGetter;
+    cat <<"EOF"
+| awk 'NF >= 1 && !/^ / {print $1}' \
+  | LC_ALL=C sort -u \
+  | awk -v st=$startnum -v nd=$numdisambig '{print $1 " " st++} END {for (i=1; i<=nd; i++) print "#" i " " st++}'
+EOF
+  ) | $this::writeToGetter num
+} # end function POGDef::errorPR::writePrettyPrinter
 
 function POGDef::table::getNLine {
   local this="$1"
